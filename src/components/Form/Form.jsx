ee9053d9c0c88732fb80f7c49c6dde072ba7a882
OlenaIa <121794911+OlenaIa@users.noreply.github.com>
2023-08-31 17:22:04 +0300

    delNanoid

--- src/components/Form/Form.jsx
+++ src/components/Form/Form.jsx
@@ -1,5 +1,5 @@
 import { useState } from "react";
-import { nanoid } from 'nanoid'
+// import { nanoid } from 'nanoid'
 import { Notify } from 'notiflix/build/notiflix-notify-aio';
 import { useDispatch, useSelector } from "react-redux";
 import { getPhoneBookValue } from "redux/phoneBookSlice";
@@ -22,8 +22,8 @@ export const Form = () => {
 
     const onSubmitAddContact = (event) => {
         event.preventDefault();
-        const data = { name, number };
-        const newObj = { ...data, id: nanoid() };
+        const newObj = { name, number };
+        // const newObj = { ...data, id: nanoid() };
 
         if (isNameNew(phoneBook, newObj) !== undefined) {
             Notify.warning(`${newObj.name} is already in contacts`, options);
